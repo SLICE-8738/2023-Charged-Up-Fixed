f3b84f8138b49f383347a7b914bbaf3591d4b3a1
romi <112425985+RomanMartin100@users.noreply.github.com>
2024-01-18 14:39:07 -0500

    Update StopShooterTest.java

--- src/main/java/frc/robot/commands/StopShooterTest.java
+++ src/main/java/frc/robot/commands/StopShooterTest.java
@@ -4,12 +4,23 @@
 
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj.PS4Controller;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.ShooterSubsystem;
 
-public class StopShooterTest extends Command {
-  /** Creates a new StopShooterTest. */
-  public StopShooterTest() {
+public class StopShooterTest extends CommandBase {
+
+  private ShooterSubsystem shooterSubsystem;
+  private PS4Controller controller;
+
+
+  /** Creates a new ShooterTest. */
+  public StopShooterTest(ShooterSubsystem shooter) {
+    shooterSubsystem = shooter;
+    this.controller = controller;
     // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(shooterSubsystem);
   }
 
   // Called when the command is initially scheduled.
@@ -18,11 +29,15 @@ public class StopShooterTest extends Command {
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    shooterSubsystem.stopSpin();
+  }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+
+  }
 
   // Returns true when the command should end.
   @Override
